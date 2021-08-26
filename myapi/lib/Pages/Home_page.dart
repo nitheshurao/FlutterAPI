7ca92447a2d941743fc08e748f71b0645d484b3f
Nithesh <nitheshurao@gmail.com>
2021-08-26 10:42:22 +0530

    .

--- myapi/lib/Pages/Home_page.dart
+++ myapi/lib/Pages/Home_page.dart
@@ -31,7 +31,7 @@ class _Home_pageState extends State<Home_page> {
     await Future.delayed(Duration(seconds: 2)); // 2 sc dely displying loding
     var CatlogJson = await rootBundle.loadString("assets/Files/Catalog.json");
     var decodedData = jsonDecode(CatlogJson);
-    // print(CatlogJson);
+    print(CatlogJson);
     var productdata = decodedData["products"];
 
     // List<Item> list =
@@ -58,19 +58,19 @@ class _Home_pageState extends State<Home_page> {
             padding: Vx.m32,
             child: Column(
               children: [
-                //  Vx.hexToColor(code)
+                //  Vx.hexToColor(code),
                 CatalogHeader(),
                 //
 
                 // if (CaltelogModels.items != null &&
-                // CaltelogModels.items.isNotEmpty)
-                // CatalogList().expand()
-                //
-                ////refre to expand use
+                //     CaltelogModels.items.isNotEmpty)
+                //   CatalogList().expand(),
 
-                // Center(
-                //   child: CircularProgressIndicator(),
-                // )
+                //refre to expand use
+
+                Center(
+                  child: CircularProgressIndicator(),
+                )
               ],
             ),
           ),
@@ -110,3 +110,92 @@ class CatalogImage extends StatelessWidget {
         .w40(context); //wodth of image inside padding
   }
 }
+
+class CatalogList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return !context.isMobile
+        ? GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, crossAxisSpacing: 20.0),
+            shrinkWrap: true,
+            itemCount: CaltelogModels.items.length,
+            itemBuilder: (context, index) {
+              final catalog = CaltelogModels.items[index];
+              return InkWell(
+                  // onTap: () => Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //     builder: (context) => HomeDetailPage(catalog: catalog),
+                  //   ),
+                  // ),
+                  // child: CatalogItem(catalog: catalog),
+                  );
+            },
+          )
+        : ListView.builder(
+            shrinkWrap: true,
+            itemCount: CaltelogModels.items.length,
+            itemBuilder: (context, index) {
+              final catalog = CaltelogModels.items[index];
+              return InkWell(
+                  // onTap: () => Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //     builder: (context) => HomeDetailPage(catalog: catalog),
+                  //   ),
+                  // ),
+                  // child: CatalogItem(catalog: catalog),
+                  );
+            },
+          );
+  }
+}
+
+// class CatalogItem extends StatelessWidget {
+//   final Item catalog;
+
+//   const CatalogItem({Key key, @required this.catalog})
+//       : assert(catalog != null),
+//         super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var children2 = [
+//       Hero(
+//         tag: Key(catalog.id.toString()),
+//         child: CatalogImage(
+//           image: catalog.image,
+//         ),
+//       ),
+//       Expanded(
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             catalog.name.text.lg.color(context.accentColor).bold.make(),
+//             catalog.desc.text.textStyle(context.captionStyle).make(),
+//             10.heightBox,
+//             ButtonBar(
+//               alignment: MainAxisAlignment.spaceBetween,
+//               buttonPadding: EdgeInsets.zero,
+//               children: [
+//                 "\$${catalog.price}".text.bold.xl.make(),
+//                 AddToCart(catalog: catalog)
+//               ],
+//             ).pOnly(right: 8.0)
+//           ],
+//         ).p(context.isMobile ? 0 : 16),
+//       )
+//     ];
+//     return VxBox(
+//       child: context.isMobile
+//           ? Row(
+//               children: children2,
+//             )
+//           : Column(
+//               children: children2,
+//             ),
+//     ).color(context.cardColor).rounded.square(150).make().py16();
+//   }
+// }
